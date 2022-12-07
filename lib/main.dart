b775dc469c2778ef0b0833fd5aa26b52f95a4db7
thefarismd <thefarismd@icloud.com>
2022-12-07 14:04:25 +0700

    cards widget, convert list to list of widgets in column

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +14,50 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'EmployeeID',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.54,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: Text('LIST OF TX'),
+            ),
+            Column(
+              children: transactions
+                  .map((transaction) => Card(
+                        child: Text(transaction.title),
+                      ))
+                  .toList(),
+            )
+          ],
+        ));
   }
 }
