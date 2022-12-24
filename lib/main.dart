725347d1ac82672243bd1d4756e9e195c4c4f536
thefarismd <thefarismd@icloud.com>
2022-12-24 12:52:02 +0800

    sizebox, container, mapping, stack widget, fractionallysizedbox widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:expense_planner/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 import './widgets/new_transaction.dart';
 import './models/transaction.dart';
+import './widgets/chart.dart';
 
 void main() => runApp(MyApp());
 
@@ -55,6 +56,18 @@ class _MyHomePageState extends State<MyHomePage> {
     //     date: DateTime.now()),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(
+            days: 7,
+          ),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewTransaction(String title, double amount) {
     final newTx = Transaction(
         id: DateTime.now().toString(),
@@ -93,14 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  // const MyWidget({Key key}) : super(key: key);
+
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentageTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPercentageTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: Color.fromRGBO(220, 220, 220, 1),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPercentageTotal,
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10))),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
